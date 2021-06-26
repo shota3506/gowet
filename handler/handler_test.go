5f54783ea52bc0d7c408dcd90b10a80f5f0f1974
shota3506 <s.shota.710.3506@gmail.com>
2021-06-26 18:45:48 +0900

    use mock for test

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -1,19 +1,64 @@
 package handler
 
 import (
+	"context"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	"github.com/shota3506/gowet/database"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestHandler_ServeHTTP(t *testing.T) {
-	d := database.NewFakeClient()
-	h := NewHandler(d)
-
 	t.Run("github.com/tenntenn/greeting", func(t *testing.T) {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		mock := database.NewMockDB(mockCtrl)
+		gomock.InOrder(
+			mock.EXPECT().
+				Get(gomock.Any(), gomock.Any()).
+				Do(func(_ context.Context, _ string) {}).
+				Return(nil, database.NewNotFoundError(errors.New("some error"))),
+			mock.EXPECT().
+				Get(gomock.Any(), gomock.Any()).
+				Do(func(_ context.Context, _ string) {}).
+				Return(nil, database.NewNotFoundError(errors.New("some error"))),
+			mock.EXPECT().
+				Set(gomock.Any(), gomock.Any(), gomock.Any()).
+				Do(func(_ context.Context, _, _ string) {}).
+				Return(nil),
+		)
+
+		h := NewHandler(mock)
+
+		req := httptest.NewRequest(
+			http.MethodGet,
+			"http://example.com/github.com/tenntenn/greeting",
+			nil,
+		)
+		recorder := httptest.NewRecorder()
+
+		h.ServeHTTP(recorder, req)
+
+		assert.Equal(t, http.StatusOK, recorder.Code)
+	})
+
+	t.Run("github.com/tenntenn/greeting cached", func(t *testing.T) {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		mock := database.NewMockDB(mockCtrl)
+		mock.EXPECT().
+			Get(gomock.Any(), gomock.Any()).
+			Do(func(_ context.Context, _ string) {}).
+			Return([]byte("{}"), nil)
+
+		h := NewHandler(mock)
+
 		req := httptest.NewRequest(
 			http.MethodGet,
 			"http://example.com/github.com/tenntenn/greeting",
@@ -27,6 +72,23 @@ func TestHandler_ServeHTTP(t *testing.T) {
 	})
 
 	t.Run("example.com", func(t *testing.T) {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		mock := database.NewMockDB(mockCtrl)
+		gomock.InOrder(
+			mock.EXPECT().
+				Get(gomock.Any(), gomock.Any()).
+				Do(func(_ context.Context, _ string) {}).
+				Return(nil, database.NewNotFoundError(errors.New("some error"))),
+			mock.EXPECT().
+				Get(gomock.Any(), gomock.Any()).
+				Do(func(_ context.Context, _ string) {}).
+				Return(nil, database.NewNotFoundError(errors.New("some error"))),
+		)
+
+		h := NewHandler(mock)
+
 		req := httptest.NewRequest(
 			http.MethodGet,
 			"http://example.com/example.com",
